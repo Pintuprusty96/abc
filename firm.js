2701e994f501a046350b5c9e9fe8438a3047bd79
pint <pintuprusty16@gmail.com>
2022-05-04 08:44:46 +0530

    pintu

---
+++ firm.js
@@ -0,0 +1,12 @@
+for(var j=1;j<=10;j++){
+var bag=""
+for(var i=1;i<=j;i++){
+    var bag=bag+i+" "
+}
+console.log(bag)
+var bag2=""
+for(var i=1;i<=j;i++){
+    var bag2=bag2+"*"+" "
+}
+console.log(bag2)
+}
